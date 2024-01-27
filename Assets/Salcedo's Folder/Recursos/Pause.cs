e394c2865bd3f3c328e47c4580eee6289eeec621
AngelSalcedo <angel-salcedo@outlook.com>
2024-01-26 23:45:42 -0600

    Pause Menu Test 1
    pause menu prefab test

---
+++ Assets/Salcedo's Folder/Recursos/Pause.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject MenuPausaObj;
+
+    public bool Ispaused = false;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+          PauseManagement();
+        }
+
+    }
+
+
+    public void PauseManagement ()
+    {
+        if (Ispaused)
+        {
+            Ispaused = false;
+            MenuPausaObj.SetActive(false);
+            Time.timeScale = 1;
+
+
+        } else
+        {
+            Ispaused = true;
+            MenuPausaObj.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ChangeScene (string Scene)
+    {
+        Manager.instance.CambioDeEscena(Scene);
+    }
+
+    public void ApplicationExit ()
+    {
+        Application.Quit();                              
+    }
+}
